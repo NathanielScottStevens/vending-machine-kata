1cd2eb82fd06b03917c874e60c39af990e61ee02
Nathaniel Scott Stevens <nathaniel.scott.stevens@gmail.com>
2016-05-05 23:22:58 -0400

    Initial commit.

---
+++ Vending Machine Kata/js/vendingMachine.js
@@ -0,0 +1,40 @@
+﻿
+
+function CoinInfo(weightInGrams, sizeInInches, weightMarginOfError, sizeMarginOfError) {
+    this.weightInGrams = weightInGrams;
+    this.sizeInInches = sizeInInches;
+    this.weightMarginOfError = weightMarginOfError;
+    this.sizeMarginOfError = sizeMarginOfError;
+
+    this.isValid = function (weight, size) {
+        var weightLowerBound = weightInGrams - weightMarginOfError;
+        var weightUpperBound = weightInGrams + weightMarginOfError;
+
+        var sizeLowerBound = sizeInInches - sizeMarginOfError;
+        var sizeUpperBound = sizeInInches + sizeMarginOfError;
+
+        var isWeightValid = weightLowerBound <= weight && weight <= weightUpperBound;
+        var isSizeValid = sizeLowerBound <= size && size <= sizeUpperBound;
+
+        return isWeightValid && isSizeValid;
+    }
+}
+
+var weightMarginOfError = 0.3;
+var sizeMarginOfError = 0.05;
+
+// https://www.usmint.gov/about_the_mint/?action=coin_specifications
+var nickel = new CoinInfo(5, 0.835, weightMarginOfError, sizeMarginOfError);
+var dime = new CoinInfo(2.268, 0.705, weightMarginOfError, sizeMarginOfError);
+var quarter = new CoinInfo(5.67, 0.955, weightMarginOfError, sizeMarginOfError);
+
+function determineCoin(weightInGrams, sizeInInches) {
+    if (nickel.isValid(weightInGrams, sizeInInches))
+        return "nickel";
+    else if (dime.isValid(weightInGrams, sizeInInches))
+        return "dime";
+    else if (quarter.isValid(weightInGrams, sizeInInches))
+        return "quarter";
+    else
+        return "other";
+}
---
+++ Vending Machine Kata/js/vendingMachineTests.js
@@ -0,0 +1,23 @@
+﻿module("determineCoin");
+
+QUnit.test("returns nickel", function(assert) {
+    assert.equal(determineCoin(nickel.weightInGrams, nickel.sizeInInches), "nickel");
+})
+
+QUnit.test("returns dime", function(assert) {
+    assert.equal(determineCoin(dime.weightInGrams, dime.sizeInInches), "dime");
+})
+
+QUnit.test("returns quarter", function(assert) {
+    assert.equal(determineCoin(quarter.weightInGrams, quarter.sizeInInches), "quarter");
+})
+
+QUnit.test("no overlap between dime and nickel", function (assert) {
+    assert.equal(determineCoin(dime.weightInGrams + weightMarginOfError, dime.sizeInInches + sizeMarginOfError), "dime");
+    assert.equal(determineCoin(nickel.weightInGrams - weightMarginOfError, nickel.sizeInInches + sizeMarginOfError), "nickel");
+})
+
+QUnit.test("no overlap between nickel and quarter", function (assert) {
+    assert.equal(determineCoin(nickel.weightInGrams + weightMarginOfError, nickel.sizeInInches + sizeMarginOfError), "nickel");
+    assert.equal(determineCoin(quarter.weightInGrams - weightMarginOfError, quarter.sizeInInches + sizeMarginOfError), "quarter");
+})
