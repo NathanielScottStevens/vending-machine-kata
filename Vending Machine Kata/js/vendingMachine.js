7d063dafd26bd8ab0415549ef5742fa0c7b5004a
Nathaniel Scott Stevens <nathaniel.scott.stevens@gmail.com>
2016-05-08 21:24:38 -0400

    Completed first draft.

--- Vending Machine Kata/js/vendingMachine.js
+++ Vending Machine Kata/js/vendingMachine.js
@@ -1,23 +1,155 @@
 ﻿var VendingMachine = function() {
 	
-	var screenDisplay;
-	var totalAmountInserted;
-
-	// The feature request requires both an "INSERT COINS" and "INSERT COIN" message.
-	// I believe this is a typo.
-	var insertCoinMessage = "INSERT COINS";
-	var thankyouMessage = "THANK YOU";
-	var soldOutMessage = "SOLD OUT";
-	var exactChangeMessage = "EXACT CHANGE ONLY";	
+	var totalAmountInserted;	
+	var currentState;
+	var coinReturn;
+	var productOutput;
 	
-	
-	function getScreenDisplay() {
-		return screenDisplay + " " + totalAmountInserted.toFixed(2);
+	// PUBLIC FUNCTIONS
+			
+	function init(nickelCount, dimeCount, quarterCount, colaCount, chipCount, candyCount){		
+		coinInventory.init(nickelCount, dimeCount, quarterCount);
+		productInventory.init(colaCount, chipCount, candyCount);
+		
+		totalAmountInserted = 0;
+		coinReturn = [];
+		productOutput = [];
+		currentState = insertCoinsState;	
 	}
 	
-	function getTotalAmountInserted() {
-		return totalAmountInserted.toFixed(2);
+	function getScreen() {
+		return currentState.display();
 	}
+	
+	function insertCoin(coin){
+		var insertedCoin = coinInventory.addCoin(coin.weight, coin.size);
+		
+		if (insertedCoin == null){
+			coinReturn.push(coin);
+		}
+		else{
+			currentState = coinsInsertedState;
+		}
+	}
+	
+	function selectProduct(selection) {
+		var product = productInventory.getProductInfo(selection);
+		
+		if (product !== undefined){
+			lastSelectedProduct = product;
+			
+			if (product.stored == 0){
+				currentState = getSoldOutState();
+			}
+			else if (product.price > totalAmountInserted){
+				currentState = getProductSelectionFailState(product);
+			}
+			else if (product.stored == 0){
+				currentState = getProductSelectionFailState(product);
+			}
+			else {
+				totalAmountInserted -= product.price;
+				currentState = getProductSelectionSuccessState();
+				
+				var outProduct = productInventory.removeProduct(selection);
+				productOutput.push(outProduct);
+				
+				if (totalAmountInserted > 0)
+					coinInventory.makeChange();
+			}	
+		}
+	}
+	
+	function returnCoins(){
+		coinInventory.makeChange();
+		currentState = insertCoinsState;
+	}
+	
+	function takeProduct() {
+		var copy = copyArray(productOutput);
+		productOutput = [];
+		return copy;
+	}
+	
+	function takeChange() {
+		var copy = copyArray(coinReturn);
+		coinReturn = [];
+		return copy;
+	}
+	
+	
+	// States
+	
+	function State(display, param){
+		this.display = display;
+		this.param = param;
+	}	
+	
+	var insertCoinsState = new State(
+		function() {
+			if (coinInventory.canMakeChange())
+				return "INSERT COINS";
+			else
+				return "EXACT CHANGE ONLY";
+		}
+	);
+	
+	var coinsInsertedState = new State(
+		function() {
+			return formatMoney(totalAmountInserted);
+		}
+	);
+	
+	function getProductSelectionSuccessState(){
+		return new State(
+			function() {
+				if (this.param){
+					this.param = false;
+					return "THANK YOU";
+				}
+				else{
+					currentState = insertCoinsState;
+					return currentState.display();
+				}
+			},
+			true
+		)
+	}
+	
+	function getProductSelectionFailState(product) {
+		return new State(
+			function() {
+				if (totalAmountInserted > 0)
+					currentState = coinsInsertedState;
+				else
+					currentState = insertCoinsState;
+				
+				return "PRICE " + formatMoney(product.price);
+			});
+	}
+	
+	function getSoldOutState(){
+		return new State(
+			function() {
+				if (this.param){
+					this.param = false;
+					return "SOLD OUT";
+				}
+				else{
+					if (totalAmountInserted > 0)
+						currentState = coinsInsertedState;
+					else
+						currentState = insertCoinsState;
+					
+					return currentState.display();
+				}
+			},
+			true
+		)
+	}
+	
+	
+	// Inventory
 	
 	/**
 	 *	Coin object.
@@ -25,10 +157,11 @@
 	 * @param {number} size - Size of the coin in millimeters.
 	 * @param {number} value - Value of coin in USD.
 	 */
-	function CoinInfo(weight, size, value) {
+	function CoinInfo(weight, size, value, stored) {
 		this.weight = weight;
 		this.size = size;
 		this.value = value;
+		this.stored = stored;
 
 		this.isValid = function (weight, size) {
 			var isWeightValid = this.weight === weight;
@@ -38,35 +171,155 @@
 		}
 	}
 	
-	// https://www.usmint.gov/about_the_mint/?action=coin_specifications
-	var nickel = new CoinInfo(5.000, 21.21, 0.05);
-	var dime = new CoinInfo(2.268, 17.91, 0.10);
-	var quarter = new CoinInfo(5.670, 24.26, 0.25);
-	
-	function insertCoin(coin){
-		var insertedCoin = determineCoin(coin.weight, coin.size);
+	// Coin specifications: https://www.usmint.gov/about_the_mint/?action=coin_specifications
+	var coinInventory = {
+		nickel: new CoinInfo(5.000, 21.21, 0.05, 0),
+	    dime: new CoinInfo(2.268, 17.91, 0.10, 0),
+	    quarter: new CoinInfo(5.670, 24.26, 0.25, 0),
 		
-		if (insertedCoin !== null)
-			totalAmountInserted += insertedCoin.value;
-	}
-
-	function determineCoin(weight, size) {
-		if (nickel.isValid(weight, size))
-			return nickel;
-		else if (dime.isValid(weight, size))
-			return dime;
-		else if (quarter.isValid(weight, size))
-			return quarter;
-		else
+		init: function(nickels, dimes, quarters) {
+			this.nickel.stored = nickels;
+			this.dime.stored = dimes;
+			this.quarter.stored = quarters;
+		},
+		
+		addCoin: function(weight, size) {
+			if (this.nickel.isValid(weight, size)) {
+				this.nickel.stored++;
+				totalAmountInserted += this.nickel.value;
+				return this.nickel;
+			}
+			else if (this.dime.isValid(weight, size)) {
+				this.dime.stored++;
+				totalAmountInserted += this.dime.value;
+				return this.dime;
+			}
+			else if (this.quarter.isValid(weight, size)) {
+				this.quarter.stored++;
+				totalAmountInserted += this.quarter.value;
+				return this.quarter;
+			}
+				
 			return null;
+		},
+		
+		removeCoin: function(coin){
+			if (coin.stored != 0){
+				coin.stored--;
+				return true;
+			}
+			else
+				return false;
+		},
+		
+		makeChange: function(){
+			if (totalAmountInserted >= 0.25 && this.quarter.stored > 0) {
+				this.removeCoin(this.quarter);
+				coinReturn.push(this.quarter);
+				totalAmountInserted -= 0.25;
+			}
+			else if(totalAmountInserted >= 0.10 && this.dime.stored > 0) {
+				this.removeCoin(this.dime);
+				coinReturn.push(this.dime);
+				totalAmountInserted -= 0.10;
+			}
+			else if (totalAmountInserted == 0.05 && this.nickel.stored > 0) {
+				this.removeCoin(this.nickel);
+				coinReturn.push(this.nickel);
+				totalAmountInserted -= 0.05;
+			}
+			
+			if (totalAmountInserted > 0)
+				this.makeChange();
+		},
+		
+		canMakeChange: function(){
+			if (this.nickel.stored >= 4)
+				return true;
+			else if (this.nickel.stored >= 2 && this.dime.stored >= 1)
+				return true;
+			else
+				return false;
+		}			
 	}
 	
-	function init(){
-		screenDisplay = insertCoinMessage;
-		totalAmountInserted = 0;
+	function Product(name, price, stored) {
+		this.name = name;
+		this.price = price;
+		this.stored = stored;
+	}
+	
+	var productInventory = {
+		cola : new Product("cola", 1.00, 0),
+		chips : new Product("chips", 0.50, 0),
+		candy : new Product("candy", 0.65, 0),
+		
+		init: function(colas, chips, candies){
+			this.cola.stored = colas;
+			this.chips.stored = chips;
+			this.candy.stored = candies;
+		},
+		
+		getProductInfo: function(product){
+			if (product == "cola"){
+				return this.cola;
+			}
+			else if (product == "chips"){
+				return this.chips;
+			}
+			else if (product == "candy"){
+				return this.candy;
+			}
+			else {
+				return null;
+			}
+		},
+		
+		removeProduct : function(product){
+			if (product == "cola"){
+				if (this.cola.stored != 0){
+					this.cola.stored--;
+					return "cola";
+				}
+			}
+			else if (product == "chips"){
+				if (this.chips.stored != 0){
+					this.chips.stored--;
+					return "chips";
+				}
+			}
+			else if (product == "candy"){
+				if (this.candy.stored != 0){
+					this.candy.stored--;
+					return "candy";
+				}
+			}
+			else{
+				return null;
+			}				
+		}
+	};
+	
+	// Helper Functions
+
+	function formatMoney(amount){
+		return "$" + amount.toFixed(2);
+	}
+	
+	function copyArray(array){
+		var copy = new Array(array.length);
+		for (var i = 0; i < copy.length; i++)
+			copy[i] = array[i];
+		return copy;
 	}
 
-	return {init:init, insertCoin:insertCoin, getScreenDisplay:getScreenDisplay, 
-				getTotalAmountInserted:getTotalAmountInserted}
+	return {
+		init: init, 
+		insertCoin: insertCoin, 
+		getScreen: getScreen,
+		selectProduct: selectProduct,
+		returnCoins: returnCoins,
+		takeProduct: takeProduct,
+		takeChange: takeChange,
+		};
 }();
-
--- Vending Machine Kata/js/vendingMachineTests.js
+++ Vending Machine Kata/js/vendingMachineTests.js
@@ -1,56 +1,219 @@
-﻿penny = { "weight": 2.500, "size": 19.05 };
-nickel = { "weight": 5.000, "size": 21.21 };
-dime = { "weight": 2.268, "size": 17.91 };
-quarter = { "weight": 5.670, "size": 24.26 };
+﻿var testObjects = {
+	penny:   { weight: 2.500, size: 19.05 },
+	nickel:   { weight: 5.000, size: 21.21 },
+	dime:    { weight: 2.268, size: 17.91 },
+	quarter: { weight: 5.670, size: 24.26 },
+	
+	chips:   { name: "chips", cost: 0.50 }
+};
+
+function defaultVendingMachineInit() {
+	VendingMachine.init(5, 5, 5, 5, 5, 5);
+}
 
 module("Screen Display")
 
 QUnit.test("Displays correct message at initializion.", function(assert) {
-	VendingMachine.init();
+	defaultVendingMachineInit();
 	
-	assert.equal(VendingMachine.getScreenDisplay(), "INSERT COINS 0.00");
+	assert.equal(VendingMachine.getScreen(), "INSERT COINS");
+});
+
+QUnit.test("Display shows THANK YOU after purchase", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	
+	assert.equal(VendingMachine.getScreen(), "THANK YOU");	
+});
+
+QUnit.test("Display shows INSERT COINS after THANK YOU", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	VendingMachine.getScreen(); // Displays THANK YOU
+	
+	assert.equal(VendingMachine.getScreen(), "INSERT COINS");	
+});
+
+QUnit.test("Displays PRICE and cost upon unsuccesful selection", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	
+	assert.equal(VendingMachine.getScreen(), "PRICE $" + testObjects.chips.cost.toFixed(2));
+	
+});
+
+QUnit.test("Next check of display after unsuccesful selection show INSERT COINS if no money has been entered.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.selectProduct(testObjects.chips.name);
+	VendingMachine.getScreen();
+	
+	assert.equal(VendingMachine.getScreen(), "INSERT COINS");
+});
+
+QUnit.test("Displays amount entered", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.nickel);
+	
+	assert.equal(VendingMachine.getScreen(), "$0.05");
+});
+
+QUnit.test("Displays INSERT COINS after returning coins.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.returnCoins();
+	
+	assert.equal(VendingMachine.getScreen(), "INSERT COINS");
+});
+
+QUnit.test("Displays SOLD OUT if selection is sold out.", function(assert) {
+	VendingMachine.init(5, 5, 5, 5, 0, 5); // No chips
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	
+	assert.equal(VendingMachine.getScreen(), "SOLD OUT");	
+});
+
+QUnit.test("Display shows amount inserted after SOLD OUT", function(assert) {
+	VendingMachine.init(5, 5, 5, 5, 0, 5); // No chips
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	VendingMachine.getScreen(); // Displays SOLD OUT
+	
+	assert.equal(VendingMachine.getScreen(), "$0.50");	
+});
+
+QUnit.test("Display EXACT CHANGE ONLY when there is no change.", function(assert) {
+	VendingMachine.init(0, 0, 0, 5, 5, 5);
+
+	assert.equal(VendingMachine.getScreen(), "EXACT CHANGE ONLY");	
+});
+
+QUnit.test("Display EXACT CHANGE ONLY when there less than 4 nickels and no dimes.", function(assert) {
+	VendingMachine.init(3, 0, 5, 5, 5, 5);
+
+	assert.equal(VendingMachine.getScreen(), "EXACT CHANGE ONLY");	
+});
+
+QUnit.test("Display EXACT CHANGE ONLY when there are 2 nickels and 1 dime.", function(assert) {
+	VendingMachine.init(3, 0, 5, 5, 5, 5);
+
+	assert.equal(VendingMachine.getScreen(), "EXACT CHANGE ONLY");	
 });
 
 
 module("Insert Coin")
 	
 QUnit.test("Accepts nickel.", function(assert) {
-	VendingMachine.init();
+	defaultVendingMachineInit();
 	
-	VendingMachine.insertCoin(nickel);
+	VendingMachine.insertCoin(testObjects.nickel);
 	
-	assert.equal(VendingMachine.getTotalAmountInserted(), 0.05);
+	assert.equal(VendingMachine.getScreen(), "$0.05");
 });
 
 QUnit.test("Accepts dime.", function(assert) {
-	VendingMachine.init();
+	defaultVendingMachineInit();
 	
-	VendingMachine.insertCoin(dime);
+	VendingMachine.insertCoin(testObjects.dime);
 	
-	assert.equal(VendingMachine.getTotalAmountInserted(), 0.10);
+	assert.equal(VendingMachine.getScreen(), "$0.10");
 });
 
 QUnit.test("Accepts quarter.", function(assert) {
-	VendingMachine.init();
+	defaultVendingMachineInit();
 	
-	VendingMachine.insertCoin(quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
 	
-	assert.equal(VendingMachine.getTotalAmountInserted(), 0.25);
+	assert.equal(VendingMachine.getScreen(), "$0.25");
 });
 
 QUnit.test("Rejects penny.", function(assert) {
-	VendingMachine.init();
+	defaultVendingMachineInit();
 	
-	VendingMachine.insertCoin(penny);
+	VendingMachine.insertCoin(testObjects.penny);
 	
-	assert.equal(VendingMachine.getTotalAmountInserted(), 0);
+	assert.equal(VendingMachine.getScreen(), "INSERT COINS");
 });
 
-QUnit.test("Rejects multiple coins and adds values to total.", function(assert) {
-	VendingMachine.init();
+QUnit.test("Deposits penny in coin return", function(assert) {
+	defaultVendingMachineInit();
 	
-	VendingMachine.insertCoin(nickel);
-	VendingMachine.insertCoin(dime)
+	VendingMachine.insertCoin(testObjects.penny);
 	
-	assert.equal(VendingMachine.getTotalAmountInserted(), 0.15);
+	assert.equal(VendingMachine.takeChange()[0], testObjects.penny);
+});
+
+
+QUnit.test("Accepts multiple coins and sums values.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.nickel);
+	VendingMachine.insertCoin(testObjects.dime)
+	
+	assert.equal(VendingMachine.getScreen(), "$0.15");
+});
+
+
+module("Select Product");
+
+QUnit.test("Returns correct product.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	
+	assert.equal(VendingMachine.takeProduct()[0], testObjects.chips.name);
+});
+
+QUnit.test("Returns nothing if inserted payment is below cost.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	
+	assert.equal(VendingMachine.selectProduct(testObjects.chips.name), null);
+});
+
+// This fails because of rounding point errors. 
+// Money needs to be stored as integers counting in cents.
+QUnit.test("Returns change after purchase.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.dime);
+	VendingMachine.selectProduct(testObjects.chips.name);
+	
+	assert.equal(VendingMachine.takeChange()[0], testObjects.dime);	
+});
+
+
+module("Coin Return");
+
+QUnit.test("Returns coins when requested.", function(assert) {
+	defaultVendingMachineInit();
+	
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.insertCoin(testObjects.quarter);
+	VendingMachine.returnCoins();
+	
+	var change = VendingMachine.takeChange();
+	assert.equal(change[0].size, testObjects.quarter.size);
+	assert.equal(change[1].size, testObjects.quarter.size);
 });
